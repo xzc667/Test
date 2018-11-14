3f35542a3a1d1d87c2c5aba703f0402090343403
xuzhichao <292358653@qq.com>
2018-11-14 18:37:21 +0800

    adf

---
+++ da/da/da.h
@@ -0,0 +1,19 @@
+//
+//  da.h
+//  da
+//
+//  Created by 许智超 on 2018/11/14.
+//  Copyright © 2018年 xzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for da.
+FOUNDATION_EXPORT double daVersionNumber;
+
+//! Project version string for da.
+FOUNDATION_EXPORT const unsigned char daVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <da/PublicHeader.h>
+
+
---
+++ da/daTests/daTests.m
@@ -0,0 +1,37 @@
+//
+//  daTests.m
+//  daTests
+//
+//  Created by 许智超 on 2018/11/14.
+//  Copyright © 2018年 xzc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface daTests : XCTestCase
+
+@end
+
+@implementation daTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
